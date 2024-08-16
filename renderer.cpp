b64d29e95e9d0a056fcf277d82cf5e1ae44f1359
Diogo Correia <diogo_correia7@hotmail.com>
2024-08-16 19:21:53 +0100

    floor and sky color

--- renderer.cpp
+++ renderer.cpp
@@ -20,6 +20,14 @@ struct Ray {
 static Ray castRay(sf::Vector2f start, float angleInDegrees, const Map &map);
 
 void Renderer::draw3dView(sf::RenderTarget &target, const Player &player, const Map &map) {
+	sf::RectangleShape rectangle(sf::Vector2f(SCREEN_W, SCREEN_H / 2.0f));
+	rectangle.setFillColor(sf::Color(100, 170, 250)); //sky color
+	target.draw(rectangle);
+
+	rectangle.setPosition(0.0f, SCREEN_H / 2.0f);
+	rectangle.setFillColor(sf::Color(70, 70, 70)); //floor color
+	target.draw(rectangle);
+
 	float angle = player.angle - PLAYER_FOV / 2.0f;
 	float angleIncrement = PLAYER_FOV / (float)NUM_RAYS;
 	float maxRenderDistance = MAX_RAYCAST_DEPTH * map.getCellSize();
